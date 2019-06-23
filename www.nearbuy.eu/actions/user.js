0cc9f515712a5a0d740a4cab6d32a461e78e7779
Felipe Reina <41518538+felipereina@users.noreply.github.com>
2019-06-23 21:10:48 +0100

    added edit profile feature to add photo and change profile information

--- www.nearbuy.eu/actions/user.js
+++ www.nearbuy.eu/actions/user.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase'
-import db from '../config/firebase'
-import { Facebook } from 'expo';
+import firebase from 'firebase';
+import db from '../config/firebase';
+import { orderBy, groupBy, values } from 'lodash'
+import { allowNotifications, sendNotification } from './'
 
 export const updateEmail = (email) => {
     return {type: 'UPDATE_EMAIL', payload: email}
@@ -14,89 +15,124 @@ export const updateUserName = (username) => {
     return {type: 'UPDATE_USERNAME', payload: username}
 }
 
-export const updateBio = (bio) => {
-    return {type: 'UPDATE_BIO', payload: bio}
+export const updateGender = (gender) => {
+    return {type: 'UPDATE_GENDER', payload: gender}
 }
 
-export const login = () =>{
-    return async (dispatch, getState) => {
-    const {email, password} = getState().user
-    try{
-        const response = await firebase.auth().signInWithEmailAndPassword(email, password)
-        dispatch(getUser(response.user.uid))
-    } catch(e){
+export const updateAge = (age) => {
+    return {type: 'UPDATE_AGE', payload: age}
+}
+
+export const updatePhoto = (photo) => {
+	return {type: 'UPDATE_PHOTO', payload: photo}
+}
+
+export const login = () => {
+	return async (dispatch, getState) => {
+		try {
+			const { email, password } = getState().user
+			const response = await firebase.auth().signInWithEmailAndPassword(email, password)
+			dispatch(getUser(response.user.uid))
+			dispatch(allowNotifications())
+		} catch (e) {
+			alert(e)
+		}
+	}
+}
+
+export const facebookLogin = () => {
+	return async (dispatch) => {
+		try {
+			const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('304682660450298')
+			if(type === 'success') {
+				// Build Firebase credential with the Facebook access token.
+				const credential = await firebase.auth.FacebookAuthProvider.credential(token);
+				// Sign in with credential from the Facebook user.
+				const response = await firebase.auth().signInWithCredential(credential)
+				const user = await db.collection('users').doc(response.uid).get()
+				if(!user.exists){
+					const user = {
+						uid: response.uid,
+						email: response.email,
+						username: response.displayName,
+                        age: '',
+                        gender: '',
+                        photo: response.photoURL,
+                        location: [],
+                        promos: [],
+						token: null,
+					}
+					db.collection('users').doc(response.uid).set(user)
+					dispatch({type: 'LOGIN', payload: user})
+				} else {
+					dispatch(getUser(response.uid))
+				}
+			}
+		} catch (e) {
+			alert(e)
+		}
+	}
+}
+
+export const getUser = (uid, type) => {
+	return async (dispatch, getState) => {
+		try {
+			const userQuery = await db.collection('users').doc(uid).get()
+			let user = userQuery.data()
+
+      let posts = []
+      const postsQuery = await db.collection('posts').where('uid', '==', uid).get()
+      postsQuery.forEach(function(response) {
+        posts.push(response.data())
+      })
+      user.posts = orderBy(posts, 'date','desc')
+
+			if(type === 'LOGIN'){
+				dispatch({type: 'LOGIN', payload: user })
+			} else {
+				dispatch({type: 'GET_PROFILE', payload: user })
+			}
+		} catch (e) {
+			alert(e)
+		}
+	}
+}
+
+export const updateUser = () => {
+    return async ( dispatch, getState )  => {
+      const { uid, username, age, gender, photo } = getState().user
+      try {
+        db.collection('users').doc(uid).update({
+          username: username,
+          age: age,
+          gender: gender,
+          photo: photo
+        })
+      } catch(e) {
         alert(e)
+      }
     }
-    } 
-}
-
-export const facebookLogin = () =>{
-    return async (dispatch) => {
-    try{
-        const { type, token } = await Facebook.logInWithReadPermissionsAsync('304682660450298', {permissions: ['public_profile']})
-        if(type === 'success'){
-        // Build Firebase credential with the Facebook access token.
-            const credential = await firebase.auth.FacebookAuthProvider.credential(token);
-
-        // Sign in with credential from the Facebook user.
-            const response = await firebase.auth().signInWithCredential(credential)
-            console.log('>>response: ',response)
-            console.log('>>response.user : ',response.user)
-            console.log('>>response.user.uid : ',response.user.uid)
-            console.log('>>response.additionalUserInfo.isNewUser : ',response.additionalUserInfo.isNewUser)
-
-            //check if the user already exists
-            const user = await db.collection('users').doc(response.user.uid).get()
-            console.log('>>user: ',user)
-             if(response.additionalUserInfo.isNewUser){
-                console.log('>>ENTROU!!! ')
-                    const user = {
-                        uid: response.user.uid,
-                        email: response.user.email,
-                        username: response.user.displayName,
-                        bio: '',
-                        photo: response.user.photoURL,
-                        token: null
-                }
-                //stores the user information on firestore database and use uid created by firebase authentication
-                db.collection('users').doc(user.uid).set(user)
-                dispatch({type: 'LOGIN', payload: user})
-            } else {
-                dispatch(getUser(response.user.uid))
-            }
-        }
-    } catch(e){
-        alert('FROM FACEBOOK METHOD: ' + e)
-    }
-    } 
-}
-
-export const getUser = (uid) =>{
-    return async (dispatch) => {
-    try{
-        const user = await db.collection('users').doc(uid).get()
-        dispatch({type: 'LOGIN', payload: user.data()})
-    } catch(e){
-        alert(e)
-    }
-    } 
-}
+  }
 
 export const signup = () =>{
     return async (dispatch, getState) => {
     try{
-        const {email, password, username, bio} = getState().user
+        const {email, password, username, age, gender} = getState().user
         const response = await firebase.auth().createUserWithEmailAndPassword(email, password)
-        
+        console.log('>>> response: ' + response)
         //create a new user object with the state input from the Text fields and uid from firebase authentication method
         if(response.user.uid){
         const user = {
             uid: response.user.uid,
             email: email,
             username: username,
-            bio: bio,
             photo: '',
-            token: null
+            age: age,
+            gender: gender,
+            location: [],
+            promos: [],
+            token: null,
+
         }
         //stores the user information on firestore database and use uid created by firebase authentication
         db.collection('users').doc(response.user.uid).set(user)
--- www.nearbuy.eu/reducers/index.js
+++ www.nearbuy.eu/reducers/index.js
@@ -1,16 +1,5 @@
 import { combineReducers } from "redux";
 
-const counter = (state = 50, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
-
 const user = (state = {}, action) => {
   switch (action.type) {
     case 'LOGIN':
@@ -21,14 +10,69 @@ const user = (state = {}, action) => {
       return {...state, password: action.payload}
     case 'UPDATE_USERNAME':
       return {...state, username: action.payload}
-    case 'UPDATE_BIO':
-      return {...state, bio: action.payload}
+    case 'UPDATE_GENDER':
+      return {...state, gender: action.payload}
+    case 'UPDATE_AGE':
+      return {...state, age: action.payload}
+    case 'UPDATE_PHOTO':
+      return { ...state, photo: action.payload}
+    case 'GET_TOKEN': 
+      return { ...state, token: action.payload}
+    default:
+      return state
+  }
+}
+
+const profile = (state = {}, action) => {
+  switch (action.type) {
+    case 'GET_PROFILE':
+      return action.payload
+    default:
+      return state
+  }
+}
+
+const post = (state = null, action) => {
+  switch (action.type) {
+    case 'UPDATE_PHOTO':
+      return { ...state, photo: action.payload }
+    case 'UPDATE_DESCRIPTION':
+      return { ...state, description: action.payload }
+    case 'UPDATE_LOCATION':
+      return { ...state, location: action.payload }
+    case 'GET_POSTS':
+      return { ...state, feed: action.payload }
+    case 'GET_COMMENTS': 
+      return { ...state, comments: action.payload }
+    default:
+      return state
+  }
+}
+
+const messages = (state = {}, action) => {
+  switch (action.type) {
+    case 'GET_MESSAGES':
+      return action.payload
+    default:
+      return state
+  }
+}
+
+const modal = (state = null, action) => {
+  switch (action.type) {
+    case 'OPEN_MODAL':
+      return { ...state, modal: action.payload }
+    case 'CLOSE_MODAL':
+      return { ...state, modal: false }
     default:
       return state
   }
 }
 
 export default combineReducers({
-  counter,
-  user
+  user,
+  post,
+  modal,
+  profile,
+  messages
 })
