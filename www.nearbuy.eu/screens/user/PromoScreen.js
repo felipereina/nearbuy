e5f92f67bbd319bfb691191e6f64499ef15c3698
rafael-oliveira25 <rafael.m.oliveira25@gmail.com>
2019-10-12 23:34:30 +0100

    Circle images done

--- www.nearbuy.eu/screens/user/PromoScreen.js
+++ www.nearbuy.eu/screens/user/PromoScreen.js
@@ -12,14 +12,19 @@ class PromoScreen extends Component {
   constructor() {
     super()
     this.state = {
-      currentPromo: ""
+      currentPromo: "",
+      currentImage: 0
     }
+
   }
+
 
   componentDidMount() {
     let promoUid = this.props.promo.currentPromo
     this.getPromo(promoUid)
   }
+
+  
 
   getPromo = async (promoUid) => {
     if (promoUid) {
@@ -30,21 +35,60 @@ class PromoScreen extends Component {
     }
   }
 
+  onScrollEnd = (e) => {
+    let contentOffset = e.nativeEvent.contentOffset;
+    let viewSize = e.nativeEvent.layoutMeasurement;
+
+    // Divide the horizontal offset by the width of the view to see which page is visible
+    let pageNum = Math.floor(contentOffset.x / viewSize.width);
+    console.log('scrolled to page ', pageNum);
+
+    this._handleImage(pageNum);
+    
+  }
+
+  _handleImage = page => {
+    this.setState({  currentImage : page });
+  };
+
   render() {
     if (this.state.currentPromo !== "") {
       console.log("PROMOSCREEN", this.state.currentPromo)
       return (
         <View style={styles.container}>
+          <View style={{ flexDirection: "row", justifyContent: "space-around", margin: 5 }}>
+            {this.state.currentPromo.photoList.map((node, x) => {
+
+              if(x == this.state.currentImage){
+                return <Image style={{ width: 30, height: 30, borderRadius: 30 / 2, alignItems: "center",borderWidth:2, borderColor: "red"}} source={{ uri: node }} key={node} />
+
+              }else {
+              return <Image style={{ width: 30, height: 30, borderRadius: 30 / 2, alignItems: "center"}} source={{ uri: node }} key={node} />
+              }
+            })}
+          </View> 
+          
+          
           <ScrollView
             horizontal
             pagingEnabled
             showsHorizontalScrollIndicator={true}
-          >
-            {this.state.currentPromo.photoList.map((node) => {
+            onMomentumScrollEnd={this.onScrollEnd}
+            >
+          
+            {this.state.currentPromo.photoList.map((node, x) => {
+              console.log(x);
               return <Image style={{ width: width, height: height - 120 }} source={{ uri: node }} key={node} />
             })}
           </ScrollView>
           <View style={{ alignItems: "center", flexDirection: "column", justifyContent: "space-between" }}>
+
+            <Text>
+
+
+            </Text>
+
+
             <TouchableOpacity style={[styles.button, { alignItems: "center", position: "relative", marginTop: 5 }]}
             onPress={() => { 
               this.props.navigation.navigate("UserQrCode")
