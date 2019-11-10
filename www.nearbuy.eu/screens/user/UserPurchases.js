0b22f93db2f2906d13e2da0d2777621b6850e7d9
rafael-oliveira25 <rafael.m.oliveira25@gmail.com>
2019-11-10 13:55:10 +0000

    Blueprint of the grid based on the Profile screen

--- www.nearbuy.eu/screens/user/UserPurchases.js
+++ www.nearbuy.eu/screens/user/UserPurchases.js
@@ -21,7 +21,47 @@ class Purchases extends React.Component {
     
         this.setState({purchases: this.props.user.purchases});
     };
+
+
+
+
+
+    //blueprint 
+    componentDidMount() {
+        this.getLikes()
+        }
     
+    getLikes = () => {
+        const {
+            likePromos
+        } = this.props.user
+        if (likePromos) {
+            likePromos.forEach(promoUid => {
+                this.updateLikes(promoUid)
+            })
+        }
+    }
+
+    updateLikes = async (promoUid) => {
+        let newElement = true
+        this.state.promos.forEach(promo => {
+            if (promo.promoId == promoUid) newElement = false
+        })
+    if (newElement) {
+            let query = await db.collection("promos")
+                .where("promoId", "==", promoUid)
+                .get()
+
+            query.forEach(promoQuery => {
+                let promo = promoQuery.data();
+                let newArray = this.state.promos
+                newArray.push(promo)
+                this.setState({
+                    promos: newArray
+                })
+            });
+        }
+    }
     render() {
         console.log('PURCHASES STATE' + this.state.purchases );
         return(
@@ -30,26 +70,57 @@ class Purchases extends React.Component {
             <Text>
                 Your purchases are: {this.state.purchases}
             </Text>
+        
+
+
+
+
+        {/* blueprint  */}
+        <FlatList
+        onRefresh={() => this.getLikes()}
+        refreshing={false}
+        style={{ paddingTop: 25, marginTop: 10 }}
+        horizontal={false}
+        numColumns={3}
+        data={this.state.promos}
+        keyExtractor={item => JSON.stringify(item.date)}
+        renderItem={({ item }) => (
+        <TouchableOpacity
+            onPress={() => {
+            this.props.setCurrentPromo(item.promoId)
+            this.props.navigation.navigate("PromoScreen")
+            }}
+        >
+        <Image
+            style={styles.squareLarge}
+            source={{ uri: item.promoPhoto }}
+        />
+        </TouchableOpacity>
+        )}
+        />
+
         </View>
+        
+        
         )
     }
 
 }
 
 const mapDispatchToProps = dispatch => {
-    return bindActionCreators({}, dispatch);
-  };
-  
-  const mapStateToProps = state => {
-    return {
-      user: state.user
+        return bindActionCreators({}, dispatch);
     };
-  };
-  
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Purchases);
+    
+    const mapStateToProps = state => {
+        return {
+        user: state.user
+        };
+    };
+    
+    export default connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(Purchases);
 
 
 
